c560df40f518462ed1e09ac8758ce33707ccaa4a
abdul_rachman <abdul.rachman@logique.co.id>
2022-03-21 17:15:37 +0700

    fix(responsve): header, etc

--- src/components/layout/header/index.js
+++ src/components/layout/header/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { Button, Box, AppBar, Toolbar, Slide, useScrollTrigger, Typography, Container, Menu, MenuItem } from '@mui/material'
+import { Button, Box, AppBar, Toolbar, Slide, useScrollTrigger, Typography, Container, Menu, MenuItem, IconButton } from '@mui/material'
 import { PAGE_TENTANG_KAMI, PAGE_INFORMASI_TERKINI, PAGE_BAITUL_MAAL, PAGE_BISNIS_ANGGOTA } from 'constanta/index'
 import ImageLogoTajir from 'assets/images/logo-tajir.png'
 import CustomLink from 'components/common/custom-link/index'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import MenuIcon from '@mui/icons-material/Menu';
+
 import DoubleArrowIcon from '@mui/icons-material/DoubleArrow';
 import { Link } from 'react-router-dom'
 
@@ -32,13 +34,93 @@ const Header = (props) => {
       setAnchorEl(null);
     };
 
+    //*
+    const [anchorElNav, setAnchorElNav] = React.useState(null);    
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };  
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+
     return (
         <>           
-            <HideOnScroll {...props}>   
-                        
-                <AppBar sx={{display: { xs: "none", sm: "none", md: "block", lg: 'block' }}}>
-                    <Toolbar >
-                        <Container>
+            <HideOnScroll {...props}>                           
+                <AppBar >
+                    <Toolbar >                                        
+                        <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            <IconButton
+                                size="large"
+                                aria-label="account of current user"
+                                aria-controls="menu-appbar"
+                                aria-haspopup="true"
+                                onClick={handleOpenNavMenu}
+                                color="inherit"
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorElNav}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'left',
+                                }}
+                                open={Boolean(anchorElNav)}
+                                onClose={handleCloseNavMenu}
+                                sx={{
+                                    display: { xs: 'block', md: 'none'},                                    
+                                }}
+                            >
+                            
+                                <MenuItem  onClick={handleCloseNavMenu}>
+                                    <CustomLink to="/"  isWhite={false}>                                              
+                                        <p className='ml-3 ml-sm-1 mr-1 text-avenir-light' > Home</p>                                               
+                                    </CustomLink>
+                                </MenuItem>     
+                                <MenuItem  onClick={handleCloseNavMenu}>
+                                    <CustomLink to={`${PAGE_TENTANG_KAMI}`}  isWhite={false}>
+                                        <p className='ml-1 mr-1 text-avenir-light' >
+                                            Tentang Kami
+                                        </p>
+                                    </CustomLink>
+                                </MenuItem>    
+                                <MenuItem  onClick={handleCloseNavMenu}>
+                                    <CustomLink to={`${PAGE_INFORMASI_TERKINI}`}  isWhite={false}>
+                                        <p className='ml-1 mr-1 text-avenir-light' >
+                                            Informasi Terkini
+                                        </p>
+                                    </CustomLink>                        
+                                </MenuItem>
+                                <MenuItem  onClick={handleCloseNavMenu}>
+                                    <CustomLink to={`${PAGE_BISNIS_ANGGOTA}`}  isWhite={false}>
+                                        <p className='ml-1 mr-1 text-avenir-light' >
+                                            Bisnis Anggota
+                                        </p>
+                                    </CustomLink>
+                                </MenuItem>
+                                <MenuItem  onClick={handleCloseNavMenu}>
+                                    <CustomLink to={`${PAGE_BAITUL_MAAL}`}  isWhite={false}>
+                                        <p className='ml-1 mr-1 text-avenir-light' >
+                                            Baitul Maal    
+                                        </p>
+                                    </CustomLink>
+                                </MenuItem>
+
+                            </Menu>
+                        </Box>
+                        <Box sx={{ mt: -1, ml: -3, flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            <img src={ImageLogoTajir} height="31" width="75" />
+                        </Box>
+
+                        <Container sx={{display: { xs: "none", sm: "none", md: "block", lg: 'block' }}}>
                             <header>                
                                 <nav className='flex flex-row'  >                                     
                                     <img src={ImageLogoTajir} height="31" width="75" className='mt-1-half' />
@@ -103,7 +185,6 @@ const Header = (props) => {
                                             </MenuItem>                                            
                                         </Menu>
                                     </Box>
-
                                 </nav>                
                             </header>
                         </Container>
