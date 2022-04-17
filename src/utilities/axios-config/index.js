4f1c7be353d72c6bda7361f161a05e99850fa103
abdul_rachman <abdul.rachman@logique.co.id>
2022-04-17 17:35:45 +0700

    fix: change to kliktajir.com

--- src/utilities/axios-config/index.js
+++ src/utilities/axios-config/index.js
@@ -4,7 +4,8 @@ const credentials = localStorage.getItem('token');
 
 const axiosConfig = axios.create({  
   // baseURL: 'http://averoa.com:8800',
-  baseURL: 'http://103.31.251.235/~kliktajir/api'
+  // baseURL: 'http://103.31.251.235/~kliktajir/api'
+  baseURL: 'https://www.kliktajir.com/~kliktajir/api'
   
 });
 
